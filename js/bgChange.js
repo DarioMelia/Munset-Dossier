4465c78f15908b9d827d4a6200cb3033417273b4
DarioMeliaBasus <dario@meliabasus.com>
2022-05-03 12:40:38 +0200

    basic layout, working menu

---
+++ js/bgChange.js
@@ -0,0 +1,62 @@
+const body = document.querySelector("body")
+const checkBox = document.querySelector("input[type='checkbox']")
+const bgDiv = document.querySelector(".bg--color")
+
+
+
+checkBox.addEventListener("click", e => {
+    body.classList.toggle("color-bg")
+})
+
+
+const menuItems = document.querySelectorAll(".menu-item a")
+menuItems.forEach(item => {
+    item.addEventListener("mouseover", e => {
+        changeBg(e.target.name)
+    })
+    // item.addEventListener("mouseleave", e =>{
+    //     setCssVar("--bg-img","linear-gradient(to right, #b24592, #f15f79)")
+    // })
+})
+
+
+function changeBg(name){
+    switch(name){
+        case "uno":
+            setAndResetBg("linear-gradient(to right, #45ffd7, #5757f4)")
+            break
+        case "dos":
+            setAndResetBg("linear-gradient(to right, #f5c842, #fa6746) ")
+            break
+        case "tres":
+            setAndResetBg("linear-gradient(to right, #a946fa, #fd7aff) ")
+            break
+        case "cuatro":
+            setAndResetBg("linear-gradient(to right, #38e05f, #38e0ca) ")
+            break
+        case "cinco":
+            setAndResetBg("linear-gradient(to right, #e81744, #ff1783) ")
+            break
+        case "seis":
+            setAndResetBg("linear-gradient(to right, #f5e662, #ade645) ")
+            break
+            
+    }
+}
+
+function setCssVar(varName, value){
+    document.documentElement.style.setProperty(varName,value)
+}
+
+function setAndResetBg(value){
+    setCssVar("--bg-img", value)
+    // bgDiv.classList.add("bg-fade-animation")
+    //         setTimeout(()=>{
+    //             setCssVar("--bg-img", value)
+    //             setTimeout(()=>{
+    //                 bgDiv.classList.remove("bg-fade-animation")
+    //             },500)
+    //         },500)
+}
+
+// document.documentElement.style.setProperty("--bg-img", )
