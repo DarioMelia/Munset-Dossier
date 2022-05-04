5337fb17d23a1052d169478e5c26b4172ffa6460
DarioMeliaBasus <dario@meliabasus.com>
2022-05-04 22:51:59 +0200

    basic audio player

---
+++ js/audio/audioPlayer.js
@@ -0,0 +1,22 @@
+const playBtn = document.getElementById("play-random")
+
+const dbf = new Audio("https://github.com/DarioMelia/Lissandra-Dossier/blob/main/js/songs/DBF.mp3?raw=true")
+const lu = new Audio("https://github.com/DarioMelia/Lissandra-Dossier/blob/main/js/songs/LU.mp3?raw=true")
+const audios = [dbf,lu]
+let isPlaying = false
+
+let prevAudio
+playBtn.addEventListener("click", e =>{
+    isPlaying=!isPlaying
+    let audio = audios[Math.floor(Math.random()*2)]
+    if(isPlaying){
+        playBtn.classList.add("playing")
+        audio.currentTime = 0
+        audio.play()
+        prevAudio = audio
+    }else{
+        playBtn.classList.remove("playing")
+        prevAudio.pause()
+    }
+
+})
--- js/audioPlayer.js
+++
@@ -1,7 +0,0 @@
-const playBtn = document.getElementById("play-random")
-
-const dbf = new Audio("DBF.mp3")
-
-playBtn.addEventListener("click", e =>{
-    dbf.play()
-})
