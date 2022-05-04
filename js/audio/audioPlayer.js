dce2afbcd3624bd6966291f1c6c5db68940c8221
DarioMeliaBasus <dario@meliabasus.com>
2022-05-04 22:52:25 +0200

    adjustments

--- js/audio/audioPlayer.js
+++ js/audio/audioPlayer.js
@@ -1,7 +1,7 @@
 const playBtn = document.getElementById("play-random")
 
-const dbf = new Audio("https://github.com/DarioMelia/Lissandra-Dossier/blob/main/js/songs/DBF.mp3?raw=true")
-const lu = new Audio("https://github.com/DarioMelia/Lissandra-Dossier/blob/main/js/songs/LU.mp3?raw=true")
+const dbf = new Audio("https://github.com/DarioMelia/Lissandra-Dossier/blob/main/js/songs/dbf.mp3?raw=true")
+const lu = new Audio("https://github.com/DarioMelia/Lissandra-Dossier/blob/main/js/songs/lu.mp3?raw=true")
 const audios = [dbf,lu]
 let isPlaying = false
 
