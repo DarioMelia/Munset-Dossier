a7bea53fbe5815ed00fc87b3ec0af61ebd30e29f
DarioMeliaBasus <dario@meliabasus.com>
2023-02-16 17:23:58 +0100

    Implement iframe lazy loading. Adjust responsivness

--- js/Gallery.js
+++ js/Gallery.js
@@ -5,18 +5,18 @@ const galeria = document.getElementById("galeria")
 const squares = galeria.querySelectorAll(".square")
 const vidSquares = galeria.querySelectorAll(".square.video")
 
-const iframeHTML = [`<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/6j1URc3EO4k"></iframe>`,
-`<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/yRV1pow2TQk"></iframe>`,
-`<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/AH3cARZh4OM"></iframe>`,
-`<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/LstgEDrQnE4"></iframe>`]
+const iframeHTML = [`<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/6j1URc3EO4k?autoplay=1"></iframe>`,
+  `<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/yRV1pow2TQk?autoplay=1"></iframe>`,
+  `<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/AH3cARZh4OM?autoplay=1"></iframe>`,
+  `<iframe class="square" allow="autoplay" allowfullscreen="true" frameborder="0" scrolling="no" src="https://www.youtube.com/embed/LstgEDrQnE4?autoplay=1"></iframe>`]
 
 // sliders[1].scrollLeft = sliders[1].scrollWidth //llevar el slider de fotos al final
 
-galeria.addEventListener("click",e=>{
-  if(!e.target.classList.contains("square")){
+galeria.addEventListener("click", e => {
+  if (!e.target.classList.contains("square")) {
     let squaresArray = [...galeria.querySelectorAll(".square")]
-    let bigSquare = squaresArray.filter(s=>s.classList.contains("big"))
-    bigSquare[0]?bigSquare[0].classList.remove("big"):null
+    let bigSquare = squaresArray.filter(s => s.classList.contains("big"))
+    bigSquare[0] ? bigSquare[0].classList.remove("big") : null
   }
 })
 
@@ -25,111 +25,128 @@ galeria.addEventListener("click",e=>{
 sliders.forEach(slider => {
   const childrenCount = slider.children.length
   setScrollWheelEvent(slider)
-  cloneAndApendChildren(slider,childrenCount,true)
-  
- 
+  cloneAndApendChildren(slider, childrenCount, true)
+
+
   slider.addEventListener("scroll", e => {
     if (e.target.clientWidth + e.target.scrollLeft >= e.target.scrollWidth) {
       console.log("Llegaste al final")
       deleteFirstChildrenAddNew(e.target, childrenCount)
       e.target.scrollLeft = 0
     } else if (e.target.scrollLeft <= 0) {
-    //   console.log("llegaste al principio")
-    //   deleteLastChildrenAddNew(e.target,childrenCount)
-    //   e.target.scrollLeft = e.target.scrollWidth
-      
+      //   console.log("llegaste al principio")
+      //   deleteLastChildrenAddNew(e.target,childrenCount)
+      //   e.target.scrollLeft = e.target.scrollWidth
+
     }
 
-  },{passive: true})
+  }, { passive: true })
 })
 
-squares.forEach(i=>{
-  if(i.classList.contains("video")){
-    i.addEventListener("click",iframeLazyLoading)
-  }else{
-    i.addEventListener("click",squaresClickHandler)
+squares.forEach(i => {
+  if (i.classList.contains("video")) {
+    i.addEventListener("click", iframeLazyLoading)
+  } else {
+    i.addEventListener("click", squaresClickHandler)
   }
-    
+
+})
+
+function cloneAndApendChildren(parent, count, atEnd) {
+  const children = parent.children
+  const childrenClones = [...children].map(child => child.cloneNode(true))
+
+  childrenClones.forEach((ch, i) => {
+    if (i < count) {
+      if (ch.classList.contains("video")) {
+        ch.addEventListener("click", iframeLazyLoading)
+      } else {
+        ch.addEventListener("click", squaresClickHandler)
+      }
+      ch.addEventListener("click", squaresClickHandler)
+      atEnd ? parent.appendChild(ch) : parent.insertBefore(ch, children[0])
+
+    }
   })
 
-  function cloneAndApendChildren(parent,count,atEnd){
-    const children = parent.children
-    const childrenClones = [...children].map(child=>child.cloneNode(true))
-  
-    childrenClones.forEach((ch,i)=>{
-        if(i<count){
-          if(ch.classList.contains("video")){
-            ch.addEventListener("click",iframeLazyLoading)
-          }else{
-            ch.addEventListener("click",squaresClickHandler)
-          }
-            ch.addEventListener("click",squaresClickHandler)
-            atEnd?parent.appendChild(ch):parent.insertBefore(ch,children[0])
-            
-          }
-    })
 
-    
-  }
+}
 
-  function deleteFirstChildrenAddNew(parent, count){
-    const children = [...parent.children]
-    cloneAndApendChildren(parent,count,true)
-    children.forEach((ch,i) => {
-      if(i<count){
-        ch.remove()
-      }
-    })
-  }
+function deleteFirstChildrenAddNew(parent, count) {
+  const children = [...parent.children]
+  cloneAndApendChildren(parent, count, true)
+  children.forEach((ch, i) => {
+    if (i < count) {
+      ch.remove()
+    }
+  })
+}
 
-  function deleteLastChildrenAddNew(parent,count){
-    const children = [...parent.children]
-    cloneAndApendChildren(parent,count,false)
-    children.forEach((ch,i) => {
-        if(i>=children.length - count){
-          ch.remove()
-        }
-      })
-  }
-
-  
+function deleteLastChildrenAddNew(parent, count) {
+  const children = [...parent.children]
+  cloneAndApendChildren(parent, count, false)
+  children.forEach((ch, i) => {
+    if (i >= children.length - count) {
+      ch.remove()
+    }
+  })
+}
 
 
-  function squaresClickHandler(e){
-    let isBig = false;
-    
-    if(e.target.classList.contains("big")) isBig = true
+
+
+function squaresClickHandler(e) {
+  let isBig = false;
+  if (!e.target.classList.contains("video")) {
+    if (e.target.classList.contains("big")) isBig = true
     let squaresArray = [...galeria.querySelectorAll(".square")]
-    let bigSquare = squaresArray.filter(s=>s.classList.contains("big"))
-    if(bigSquare){
-    bigSquare.forEach(s=>{
-      s.classList.remove("big")
-    })}
-    if(!isBig)e.target.classList.add("big")
+    let bigSquare = squaresArray.filter(s => s.classList.contains("big"))
+    if (bigSquare) {
+      bigSquare.forEach(s => {
+        s.classList.remove("big")
+      })
+    }
+    if (!isBig) e.target.classList.add("big")
   }
+}
 
-  function setScrollWheelEvent(slider){
-    let timer
-    slider.addEventListener("wheel", e => {
-      slider.classList.add("no-scroll-snap")
-      if (timer) clearTimeout(timer)
-      if (e.deltaY > 0) slider.scrollLeft += 100
-      else slider.scrollLeft -= 100
-      timer = setTimeout(() => {
-        slider.classList.remove("no-scroll-snap")
-      }, 300)
-    },{passive: true})
-  }
+function setScrollWheelEvent(slider) {
+  let timer
+  slider.addEventListener("wheel", e => {
+    slider.classList.add("no-scroll-snap")
+    if (timer) clearTimeout(timer)
+    if (e.deltaY > 0) slider.scrollLeft += 100
+    else slider.scrollLeft -= 100
+    timer = setTimeout(() => {
+      slider.classList.remove("no-scroll-snap")
+    }, 300)
+  }, { passive: true })
+}
 
-  function iframeLazyLoading(e){
-    const i = parseInt(this.getAttribute("data-idx"))
-    // this.removeEventListener("click",iframeLazyLoading,false)
-    this.style = ""
-    this.classList.remove("square")
-    this.innerHTML = iframeHTML[i]
-    console.log("aquí")
-  }
+function iframeLazyLoading(e) {
+  const i = parseInt(this.getAttribute("data-idx"))
+  const prevState = this.cloneNode(true)
+  this.removeEventListener("click", iframeLazyLoading, false)
+  this.style = ""
+  this.innerHTML = iframeHTML[i]
+  this.addEventListener("mouseleave", e => mouseLeaveHandler(e, prevState, i))
+  console.log("pinchaste, wei")
+}
+
+function mouseLeaveHandler(e, prev, i) {
+  setTimeout(() => {
+    e.target.parentNode.replaceChild(prev, e.target)
+    console.log(e.target.getAttribute("data-idx"))
+    const newNodes = document.querySelectorAll(`.square[data-idx="${i}"]`)
+    newNodes.forEach(node=>node.addEventListener("click", iframeLazyLoading))
+  }, 200)
 
 
-  
-  
+
+
+}
+
+
+
+
+
