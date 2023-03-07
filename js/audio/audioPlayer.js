2535250725bce607e5483e063771c8148808388e
DarioMeliaBasus <dario@meliabasus.com>
2023-03-07 18:07:18 +0100

    Add spoty player, menu interactions through history api

--- js/audio/audioPlayer.js
+++ js/audio/audioPlayer.js
@@ -7,26 +7,26 @@ const audios = [dbf,lu]
 let isPlaying = false
 
 let prevAudio
-// playBtn.addEventListener("click", e =>{
-//     isPlaying=!isPlaying
-//     let audioIdx = Math.floor(Math.random()*2)
-//     let audio = audios[audioIdx]
-//     if(audio === prevAudio){
-//         audioIdx
-//     }
-//     if(isPlaying){
-//         playBtn.classList.add("playing")
-//         audio.currentTime = 0
-//         audio.play()
-//         prevAudio = audio
-//     }else{
-//         playBtn.classList.remove("playing")
-//         prevAudio.pause()
-//     }
+playBtn.addEventListener("click", e =>{
+    isPlaying=!isPlaying
+    let audioIdx = Math.floor(Math.random()*2)
+    let audio = audios[audioIdx]
+    if(audio === prevAudio){
+        audioIdx
+    }
+    if(isPlaying){
+        playBtn.classList.add("playing")
+        audio.currentTime = 0
+        audio.play()
+        prevAudio = audio
+    }else{
+        playBtn.classList.remove("playing")
+        prevAudio.pause()
+    }
 
-//     audio.addEventListener("ended", e=>{
-//         playBtn.classList.remove("playing")
-//         isPlaying= false
-//     })
+    audio.addEventListener("ended", e=>{
+        playBtn.classList.remove("playing")
+        isPlaying= false
+    })
 
-// })
+})
